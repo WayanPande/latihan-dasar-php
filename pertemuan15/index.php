1467c3ad8ca98d5bf334306affb229c47013a62e
WayanPande <yande554@gmail.com>
2020-08-26 21:31:48 +0800

    Menambahkan fungsi Pagination pada index.php

--- pertemuan15/index.php
+++ pertemuan15/index.php
@@ -2,13 +2,22 @@
 
 session_start();
 
-if (!isset($_SESSION["login"])){
-    header ("Location: login.php");
+if (!isset($_SESSION["login"])) {
+    header("Location: login.php");
 }
 
 //koneksi ke database
 require 'functions.php';
-$mahasiswa = query("SELECT * FROM mahasiswa");
+
+// pagination
+// konfigurasi
+$jumlahDataPerhalaman = 2;
+$jumlahData = count(query("SELECT * FROM mahasiswa"));
+$jumlahHalaman = ceil($jumlahData / $jumlahDataPerhalaman);
+$halamanAktif = (isset($_GET["halaman"])) ? $_GET["halaman"] : 1;
+$awalData = ($jumlahDataPerhalaman * $halamanAktif) - $jumlahDataPerhalaman;
+
+$mahasiswa = query("SELECT * FROM mahasiswa LIMIT $awalData, $jumlahDataPerhalaman");
 
 // jika tombol cari diitekan
 if (isset($_POST["cari"])) {
@@ -19,11 +28,13 @@ if (isset($_POST["cari"])) {
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
- <meta charset="UTF-8">
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Halaman Admin</title>
 </head>
+
 <body>
 
     <a href="logout.php">Logout</a>
@@ -35,11 +46,31 @@ if (isset($_POST["cari"])) {
 
     <form action="" method="post">
 
-        <input type="text" name="keyword" size="40" autofocus 
-        placeholder="Masukan keyword pencarian" autocomplete="off">
+        <input type="text" name="keyword" size="40" autofocus placeholder="Masukan keyword pencarian" autocomplete="off">
         <button type="submit" name="cari">Cari</button>
-        
+
     </form>
+    <br>
+    <!-- navigasi -->
+
+    <?php if ($halamanAktif != 1) :?>
+    <a href="?halaman=<?= $halamanAktif - 1;?>">&laquo;</a>
+    <?php endif;?>
+
+
+
+    <?php for ($i = 1; $i <= $jumlahHalaman; $i++) : ?>
+        <?php if ($i == $halamanAktif) : ?>
+            <a href="?halaman=<?= $i; ?>" style="font-weight: bold; color: red;"><?= $i ?></a>
+        <?php else : ?>
+            <a href="?halaman=<?= $i; ?>"><?= $i ?></a>
+        <?php endif; ?>
+    <?php endfor; ?>
+    
+    <?php if ($halamanAktif != $jumlahHalaman): ?>
+    <a href="?halaman=<?= $halamanAktif + 1;?>">&raquo;</a>
+    <?php endif;?>
+
     <br>
     <table border="1" cellpadding="10" cellspacing="0">
         <tr>
@@ -51,22 +82,23 @@ if (isset($_POST["cari"])) {
             <th>email</th>
             <th>jurusan</th>
         </tr>
-        <?php $i = 1;?>
-        <?php foreach($mahasiswa as $row):?>
-        <tr>
-            <td><?= $i;?></td>
-            <td>
-                <a href="ubah.php?id=<?= $row["id"]?>">ubah</a> |
-                <a href="hapus.php?id=<?= $row["id"]?>" onclick="return confirm('yakin?');">hapus</a>
-            </td>
-            <td><img src="img/<?= $row["gambar"]?>" width="50"></td>
-            <td><?= $row["nim"]?></td>
-            <td><?= $row["nama"]?></td>
-            <td><?= $row["email"]?></td>
-            <td><?= $row["jurusan"]?></td>
-        </tr>
-        <?php $i++;?>
-        <?php endforeach;?>
-    </tabel>
+        <?php $i = 1; ?>
+        <?php foreach ($mahasiswa as $row) : ?>
+            <tr>
+                <td><?= $i; ?></td>
+                <td>
+                    <a href="ubah.php?id=<?= $row["id"] ?>">ubah</a> |
+                    <a href="hapus.php?id=<?= $row["id"] ?>" onclick="return confirm('yakin?');">hapus</a>
+                </td>
+                <td><img src="img/<?= $row["gambar"] ?>" width="50"></td>
+                <td><?= $row["nim"] ?></td>
+                <td><?= $row["nama"] ?></td>
+                <td><?= $row["email"] ?></td>
+                <td><?= $row["jurusan"] ?></td>
+            </tr>
+            <?php $i++; ?>
+        <?php endforeach; ?>
+        </tabel>
 </body>
+
 </html>
