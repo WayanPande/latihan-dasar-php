250e046cd04ff76ffd17f6298811c787a10ffc7c
WayanPande <yande554@gmail.com>
2020-08-26 20:15:48 +0800

    Menambahkan fitur cookie

--- pertemuan14/logout.php
+++ pertemuan14/logout.php
@@ -5,6 +5,7 @@ $_SESSION = [];
 session_unset();
 session_destroy();
 
-header('Location: login.php');
+setcookie('id', '', time() - 3600);
+setcookie('key', '', time() - 3600);
 
-?>
+header('Location: login.php');
