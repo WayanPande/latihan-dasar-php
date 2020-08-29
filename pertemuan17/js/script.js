8fb515d997249f39f0bc7346121d65d6b4d48d0a
WayanPande <yande554@gmail.com>
2020-08-29 20:39:48 +0800

    Mengubah semua script menjadi script jquery

--- pertemuan17/js/script.js
+++ pertemuan17/js/script.js
@@ -1,24 +1,22 @@
-// ambil elemen2 yg dibutuhkan
+$(document).ready(function() {
+    // hilangkan tombol cari
+    $('#tombol-cari').hide();
 
-var keyword = document.getElementById('keyword');
-var tombolCari = document.getElementById('tombol-cari');
-var container = document.getElementById('container');
+    // event ketika keyword ditulis
+    $('#keyword').on('keyup', function() {
+        // munculkan icon loading
+        $('.loader').show();
 
-// tambahkan event ketika keyword ditulis
-keyword.addEventListener('keyup', function () {
+
+        // // ajax menggunakan load
+        // $('#container').load('ajax/mahasiswa.php?keyword=' + $('#keyword').val());
+        
+
+        $.get('ajax/mahasiswa.php?keyword=' + $('#keyword').val(), function (data) {
+
+            $('#container').html(data);
+            $('.loader').hide();
+        });
     
-    // buat object ajax
-    var xhr = new XMLHttpRequest();
-
-    // cek kesiapan ajax
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState == 4 && xhr.status == 200){
-            container.innerHTML = xhr.responseText;
-        }
-    }
-
-    // eksekusi ajax
-    xhr.open('GET', 'ajax/mahasiswa.php?keyword=' + keyword.value, true);
-    xhr.send();
-
-})
+    });
+});
